048e1634ccfb91ecf28d6bdec08b01041a75321c
Mihail Chesnokov <mike7105@mail.ru>
2026-04-19 16:53:21 +0300

    Update main.py

--- youtube/main.py
+++ youtube/main.py
@@ -1,12 +1,22 @@
 """скачивает видео с youtube
-https://pytube.io/en/latest/api.html"""
-from pytube import Playlist
+https://pytube3.readthedocs.io/en/latest/user/quickstart.html"""
+# from pytube import Playlist
+from pytube import YouTube
+
 
 if __name__ == '__main__':
-    p = Playlist('https://www.youtube.com/watch?v=99RhirZkT7s&list=PLC8azx2wuTWjsaLbf6uPUKxRoO-A-y5wb')
-    print(f'Downloading: {p.title}, videos: {p.length}')
-    for i, video in enumerate(p.videos):
-        # video.streams.first().download()
-        ys = video.streams.get_highest_resolution()
-        print(f'{i+1} Downloading: {ys.title}')
-        ys.download(r"N:\мультфильмы\лёва")
+    # YouTube('https://youtu.be/6KwLpnCdQiA').streams.first().download(r"C:\Users\25430\Downloads\new")
+    yt = YouTube('https://www.youtube.com/watch?v=6KwLpnCdQiA')
+    print(yt.streams.all())
+
+
+
+          # .streams.get_highest_resolution().download(r"C:\Users\25430\Downloads\new"))
+
+    # p = Playlist('https://youtu.be/6KwLpnCdQiA')
+    # print(f'Downloading: {p.title}, videos: {p.length}')
+    # for i, video in enumerate(p.videos):
+    #     # video.streams.first().download()
+    #     ys = video.streams.get_highest_resolution()
+    #     print(f'{i+1} Downloading: {ys.title}')
+    #     ys.download(r"C:\Users\25430\Downloads\new")
